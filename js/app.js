45e9bb836b1ba06cfee821466d68fe7dc5327831
NazarenoDersho <nazarenodersho@gmail.com>
2023-07-17 16:52:29 -0300

    Cambios en el codigo JavaScript (app.js)

--- js/app.js
+++ js/app.js
@@ -1,41 +1,39 @@
 console.log("Starting");
 
+let generatedCard;
+
 document.getElementById("cardForm").addEventListener("submit", function(event) {
     event.preventDefault();
 
-//  Valores del formulario:
+    // Valores del formulario:
+    let name = document.getElementById("name").value;
+    let title = document.getElementById("title").value;
+    let contact = document.getElementById("contact").value;
 
-let name = document.getElementById("name").value;
-let title = document.getElementById("title").value;
-let contact = document.getElementById("contact").value;
+    // JSON:
+    let cardData = {
+        name: name,
+        title: title,
+        contact: contact
+    }
 
-//JSON:
-let cardData = {
-    name: name,
-    title, title,
-    contact: contact
-}
+    let cardDataJson = JSON.stringify(cardData);
 
-let cardDataJson = JSON.stringify(cardData);
+    // LocalStorage:
+    localStorage.setItem("cardData", cardDataJson);
 
-// LocalStorage:
-
-localStorage.setItem("cardData", cardDataJson);
-
-generateCard();
-
+    generatedCard = generateCard();
+    displayCard(generatedCard);
 });
 
 // Generar tarjeta:
-
 function generateCard() {
     let cardDataJson = localStorage.getItem("cardData");
-    
-    if(cardDataJson) {
+
+    if (cardDataJson) {
         let cardData = JSON.parse(cardDataJson);
 
         // Tarjeta de presentacion:
-
         let card = document.createElement("div");
         card.classList.add("card");
 
@@ -52,11 +50,52 @@ function generateCard() {
         card.appendChild(titleText);
         card.appendChild(contactText);
 
-        // Agregamos la tarjeta en el contenedor:
+        return card;
+    }
+    return null;
+}
+
+// Mostrar tarjeta en el contenedor:
+function displayCard(card) {
+    if (card) {
         let cardContainer = document.getElementById("cardContainer");
         cardContainer.innerHTML = "";
         cardContainer.appendChild(card);
     }
+}
+
+// Obtener todas las tarjetas almacenadas en localStorage:
+function getAllCards() {
+    let cards = [];
+    for (let i = 0; i < localStorage.length; i++) {
+        let key = localStorage.key(i);
+        let cardDataJson = localStorage.getItem(key);
+        let cardData = JSON.parse(cardDataJson);
+        cards.push(cardData);
+    }
+    return cards;
+}
+
+// Sort:
+
+function sortCardsByNameAsc() {
+    let cards = getAllCards();
+    cards.sort((a, b) => a.name.localeCompare(b.name));
+    return cards;
+}
+
+// Filter:
+
+function filterCardsByTitle(filterText) {
+    let cards = getAllCards();
+    return cards.filter(card => card.title.toLowerCase().includes(filterText.toLowerCase()));
+}
+
+// Find:
+
+function findCardByName(name) {
+    let cards = getAllCards();
+    return cards.find(card => card.name === name);
 }
 
 generateCard();
